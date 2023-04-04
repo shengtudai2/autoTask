b1d5282a3a3317a2f68583a0bd6a0a445b8d25e4
shengtudai2 <85939151+shengtudai2@users.noreply.github.com>
2023-04-04 22:51:16 +0800

    Update miiow_signin.py
    add qq notify

--- miiow_signin.py
+++ miiow_signin.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import os
+from utils.notify import private_notice
 
 now_utc = datetime.datetime.utcnow()
 
@@ -11,6 +12,9 @@ beijing_time = now_utc + datetime.timedelta(hours=8)
 beijing_date = beijing_time.date().strftime('%Y-%m-%d')
 
 pwd = os.environ['API_PWD']
+nty_url = os.environ["NOTIFY"]
+qq = os.environ["QQ"]
+
 
 token = requests.get(f'http://47.100.47.189:10010/get_token/?pwd={pwd}').json()['access_token']
 
@@ -21,5 +25,7 @@ headers = {
 }
 
 a = requests.post(url, headers=headers)
+
 msg = a.json()['message']
-print(msg)
+msg = "【猫人】\n" + msg
+private_notice(msg=msg, url=nty_url, qq=qq)
