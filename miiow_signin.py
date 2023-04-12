a412a0238ab8160adf2705d861803b0a06572cde
shengtudai2 <85939151+shengtudai2@users.noreply.github.com>
2023-04-12 11:11:49 +0800

    Update miiow_signin.py
    添加多账号

--- miiow_signin.py
+++ miiow_signin.py
@@ -16,18 +16,20 @@ nty_url = os.environ["NOTIFY"]
 qq = os.environ["QQ"]
 
 
-token = requests.get(f'http://47.100.47.189:10010/get_token/?pwd={pwd}').json()['access_token']
-
-url = 'https://shopapp.miiow.com.cn/buyer/members/sign?time=' + beijing_date
-
-headers = {
-    'accessToken': token
-}
-
-a = requests.post(url, headers=headers)
-
-jf = requests.get("https://shopapp.miiow.com.cn/buyer/member/memberPointsHistory/getMemberPointsHistoryVO", headers=headers).json()["result"]["allPoint"]
-
-msg = a.json()['message']
-msg = "【猫人签到】\n" + msg + "\n总积分：" + str(jf)
+tokens = requests.get(f'http://47.100.47.189:10010/get_token/?pwd={pwd}').json()['access_token']
+jfs = []
+msgs = []
+for token in tokens:
+    url = 'https://shopapp.miiow.com.cn/buyer/members/sign?time=' + beijing_date
+    headers = {
+        'accessToken': token
+    }
+    a = requests.post(url, headers=headers)
+    jf = requests.get("https://shopapp.miiow.com.cn/buyer/member/memberPointsHistory/getMemberPointsHistoryVO", headers=headers).json()["result"]["allPoint"]
+    jfs.append(jf)
+    msgs.append(a.json()['message'])
+        
+msg = "【猫人签到】\n"
+for _ in range(len(msgs)):
+    msg += f"账号{_ + 1}---{msgs[_]}---{jfs[_]}分\n"
 private_notice(msg=msg, url=nty_url, qq=qq)
